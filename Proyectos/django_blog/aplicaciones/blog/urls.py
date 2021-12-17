e6c804e74660fd13d8d38f25be0e343b08ae2055
ivansanchez90 <sanchezoliva.ivan@gmail.com>
2021-12-17 20:55:35 -0300

    agregado login y logout y enlazados

--- Proyectos/django_blog/aplicaciones/blog/urls.py
+++ Proyectos/django_blog/aplicaciones/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import LoginView, LogoutView
+
 urlpatterns = [
-   
     path('',home, name = 'index'),
     path('register/',register, name='register'),
+    path('login/',LoginView.as_view(template_name='usuarios/login.html'), name="login"),
+    path('logout/',LogoutView.as_view(template_name='usuarios/logout.html'), name="logout"),
     path('generales/',generales, name = 'generales'),
     path('accion_clima/',accionclima, name='accion_clima'),
     path('agua_limpia/',agualimpia, name='agua_limpia'),
--- Proyectos/django_blog/aplicaciones/blog/views.py
+++ Proyectos/django_blog/aplicaciones/blog/views.py
@@ -70,6 +70,7 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data['username']
             messages.success(request, f'Usuario {username} creado')
             return redirect('/')

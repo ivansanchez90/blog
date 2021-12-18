1b97bcfd332f4afd1aee5d3b52d4bd4ea26ea35f
ivansanchez90 <sanchezoliva.ivan@gmail.com>
2021-12-17 21:55:21 -0300

    filtros aplicados a categorias

--- Proyectos/django_blog/aplicaciones/blog/forms.py
+++ Proyectos/django_blog/aplicaciones/blog/forms.py
@@ -1,63 +1,7 @@
 from django import forms
 from django.contrib.auth.forms  import UserCreationForm
 from django.contrib.auth.models import User
-# class FormularioLogin(AuthenticationForm):
-#     def __init__(self, *args, **kwargs):
-#         super(FormularioLogin, self).__init__(*args,**kwargs)
-#         self.fhields['username'].widget.attrs['class'] = 'form-control'
-#         self.fhields['username'].widget.attrs['placeholder'] = 'Nombre de Usuario'
-#         self.fhields['password'].widget.attrs['class'] = 'form-control'
-#         self.fhields['password'].widget.attrs['placeholder'] = 'Contraseña'
 
-# class FormularioUsuario(forms.ModelForm):
-#     password1 = forms.CharField(label = 'Contraseña', widget=forms.PasswordInput(
-#         attrs = {
-#             'class': 'form-control',
-#             'placeholder': 'Ingrese su contraseña...',
-#             'id': 'password1',
-#             'required': 'required',
-#         }
-#     ))
-
-#     passwrod2 = forms.CharField(label = 'Contraseña de Confirmación', widget=forms.PasswordInput(
-#         attrs = {
-#             'class': 'form-control',
-#             'placeholder': 'Ingrese nuevamente su contraseña...',
-#             'id': 'password2',
-#             'required': 'required',
-#         }
-#     ))
-
-#     class Meta:
-#         modelo = Autor
-#         fields = ('email', 'username', 'nombres', 'apellidos')
-#         widgets = {
-#             'email' : forms.EmailInput(
-#                 attrs = {
-#                     'class': 'form-control',
-#                     'placeholder' : 'Correo Electrónico'
-#                 }
-#             ),
-#             'nombres' : forms.TextInput(
-#                 attrs = {
-#                     'class': 'form-control',
-#                     'placeholder' : 'Ingrese sus nombres'
-#                 }
-#             ),
-#             'apellidos' : forms.TextInput(
-#                 attrs = {
-#                     'class': 'form-control',
-#                     'placeholder' : 'Ingrese sus apellidos'
-#                 }
-#             ),
-#             'username' : forms.TextInput(
-#                 attrs= {
-#                     'class': 'form-control',
-#                     'placeholder' : 'Ingrese sus nombre de usuario'
-#                 }
-#             )
-#         }
-        
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
--- Proyectos/django_blog/aplicaciones/blog/views.py
+++ Proyectos/django_blog/aplicaciones/blog/views.py
@@ -2,67 +2,101 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
+from .models import Post, Categoria
 
-
-def get_queryset(self):
-    return self.objects.filter(estado=True)
 
 
 def home(request):
-    return render(request, "index.html")
+    posts = Post.objects.filter(estado = True)
+    context = {'posts':posts}
+    return render(request, "index.html",context)
 
 def generales(request):
     return render(request, "categorias/generales.html")
 
 def accionclima(request):
+    posts = Post.objects.filter(estado = True, 
+    categoria = Categoria.objects.get(nombre = 'Acción por el Clima'))
     return render(request, "categorias/accion_clima.html")
 
 def agualimpia(request):
+    posts = Post.objects.filter(estado = True, 
+    categoria = Categoria.objects.get(nombre = 'Agua Limpia y Saneamiento'))
     return render(request, "categorias/agua_limpia.html")
 
 def alianzaobjetivos(request):
+    posts = Post.objects.filter(estado = True, 
+    categoria = Categoria.objects.get(nombre = 'Alianzas Para Lograr Los Objetivos'))
     return render(request, "categorias/alianza_objetivos.html")
 
 def ciudadessostenibles(request):
+    posts = Post.objects.filter(estado = True, 
+    categoria = Categoria.objects.get(nombre = 'Ciudades y Comunidades Sostenibles'))
     return render(request, "categorias/ciudades_sostenibles.html")
 
 def educacioncalidad(request):
+    posts = Post.objects.filter(estado = True, 
+    categoria = Categoria.objects.get(nombre = 'Educación de Calidad'))
     return render(request, "categorias/educacion_calidad.html")
 
 def energiasostenible(request):
+    posts = Post.objects.filter(estado = True, 
+    categoria = Categoria.objects.get(nombre = 'Energía Sostenible y No Contaminable'))
     return render(request, "categorias/energia_sostenible.html")
 
 def finpobreza(request):
+    posts = Post.objects.filter(estado = True, 
+    categoria = Categoria.objects.get(nombre = 'Fin de la pobreza'))
     return render(request, "categorias/fin_pobreza.html")
 
 def hambrecero(request):
+    posts = Post.objects.filter(estado = True, 
+    categoria = Categoria.objects.get(nombre = 'Hambre Cero'))
     return render(request, "categorias/hambre_cero.html")
 
 def igualdadgenero(request):
+    posts = Post.objects.filter(estado = True, 
+    categoria = Categoria.objects.get(nombre = 'Igualdad de Género'))
     return render(request, "categorias/igualdad_genero.html")
 
 def industria(request):
+    posts = Post.objects.filter(estado = True, 
+    categoria = Categoria.objects.get(nombre = 'Industria, Innovación e Infraestructura'))
     return render(request, "categorias/industria.html")
 
 def pazjusticia(request):
+    posts = Post.objects.filter(estado = True, 
+    categoria = Categoria.objects.get(nombre = 'Paz, Justicia e Instituciones Sólidas'))
     return render(request, "categorias/paz_justicia.html")
 
 def produccionconsumo(request):
+    posts = Post.objects.filter(estado = True, 
+    categoria = Categoria.objects.get(nombre = 'Producción y Consumo Responsable'))
     return render(request, "categorias/produccion_consumo.html")
 
 def reducciondesigualdad(request):
+    posts = Post.objects.filter(estado = True, 
+    categoria = Categoria.objects.get(nombre = 'Reducción de las Desigualdades'))
     return render(request, "categorias/reduccion_desigualdad.html")
 
 def saludbienestar(request):
+    posts = Post.objects.filter(estado = True, 
+    categoria = Categoria.objects.get(nombre = 'Salud y Bienestar'))
     return render(request, "categorias/salud_bienestar.html")
 
 def trabajodecente(request):
+    posts = Post.objects.filter(estado = True, 
+    categoria = Categoria.objects.get(nombre = 'Trabajo Decente y Crecimiento Económico'))
     return render(request, "categorias/trabajo_decente.html")
 
 def vidaecosistema(request):
+    posts = Post.objects.filter(estado = True, 
+    categoria = Categoria.objects.get(nombre = 'Vida de Ecosistemas Terrestres'))
     return render(request, "categorias/vida_ecosistema.html")
 
 def vidasubmarina(request):
+    posts = Post.objects.filter(estado = True, 
+    categoria = Categoria.objects.get(nombre = 'Vida Submarina'))
     return render(request, "categorias/vida_submarina.html")
 
 

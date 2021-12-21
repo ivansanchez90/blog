872bc7cdec4eecf36152319edd1b13c70cf1a20e
ivansanchez90 <sanchezoliva.ivan@gmail.com>
2021-12-21 00:18:07 -0300

    preparando para deploy

--- Proyectos/django_blog/django_blog/settings/base.py
+++ Proyectos/django_blog/django_blog/settings/base.py
@@ -68,17 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_blog.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]

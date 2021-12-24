f17fa942ec821a44c7f6d3ebd1f3e8e3b86935f7
ivansanchez90 <sanchezoliva.ivan@gmail.com>
2021-12-24 04:39:23 -0300

    subiendo a la rama main

--- Proyectos/django_blog/aplicaciones/blog/forms.py
+++ Proyectos/django_blog/aplicaciones/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from .models import Comment, Post
+from .models import Comment, Post, User
 from django.contrib.auth.forms  import UserCreationForm
-from django.contrib.auth.models import User
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
